904c04b5f7cc9abe6a61f2ed75b61574b9fe8a38
Jose Torres <jose.torres@runtastic.com>
2018-04-24 18:25:56 +0200

    Adds ngxelectron module for angular electron interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxElectronModule } from 'ngx-electron';
 import { NgModule } from '@angular/core';
 
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxElectronModule
   ],
   providers: [],
   bootstrap: [AppComponent]
